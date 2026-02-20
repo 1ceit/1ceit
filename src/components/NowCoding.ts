fed9b4770f92475d0d2b45b57c9f4a680f88aa32
CJ <C.j.a.34a@gmail.com>
2026-02-20 13:33:39 -0500

    fix(now-coding): fix unclosed htm tag causing 500 error

--- src/components/NowCoding.ts
+++ src/components/NowCoding.ts
@@ -12,6 +12,26 @@ export interface Props {
 
 export const NowCoding = ({ isCoding, fileName, language, iconUrl, gitUrl }: Props) => {
 
+  const inner = html`
+    <img src="${iconUrl}" width="48" height="48" style=${{ borderRadius: 3, marginLeft: 16 }} />
+    <div
+      style=${{
+      display: "flex",
+      flex: 1,
+      flexDirection: "column",
+      marginTop: -4,
+      marginLeft: 16,
+    }}
+    >
+      <${Text} weight="bold">
+        ${isCoding ? `Editing ${fileName}` : "Not currently coding"}
+      </${Text}>
+      <${Text} color="${!isCoding ? "gray" : undefined}">
+        ${isCoding ? `VS Code • ${language}` : "Visual Studio Code"}
+      </${Text}>
+    </div>
+  `;
+
   return html`
     <${ReadmeImg} width="540" height="64">
       <div
@@ -22,25 +42,9 @@ export const NowCoding = ({ isCoding, fileName, language, iconUrl, gitUrl }: Pro
       paddingLeft: 4,
     }}
       >
-        <img src="${iconUrl}" width="48" height="48" style=${{ borderRadius: 3, marginLeft: 16 }} />
-        ${gitUrl ? html`<a href="${gitUrl}" target="_blank" style=${{ textDecoration: "none" }}>` : ''}
-        <div
-          style=${{
-      display: "flex",
-      flex: 1,
-      flexDirection: "column",
-      marginTop: -4,
-      marginLeft: 16,
-    }}
-        >
-          <${Text} weight="bold">
-            ${isCoding ? `Editing ${fileName}` : "Not currently coding"}
-          </${Text}>
-          <${Text} color="${!isCoding ? "gray" : undefined}">
-            ${isCoding ? `VS Code • ${language}` : "Visual Studio Code"}
-          </${Text}>
-        </div>
-        ${gitUrl ? html`</a>` : ''}
+        ${gitUrl && gitUrl !== ""
+      ? html`<a href="${gitUrl}" target="_blank" style=${{ textDecoration: "none", display: "flex", flex: 1, alignItems: "center" }}>${inner}</a>`
+      : inner}
       </div>
     </${ReadmeImg}>
   `;
